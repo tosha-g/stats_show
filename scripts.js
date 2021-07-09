02d6c0e239359ebfa028c12a00b8b581a2e48fd5
tosha-g <vetagreenstone@gmail.com>
2021-07-09 23:07:10 +0300

    July 9 update with JS, fixed boxes, and header

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function getborrowAmount(){
+    const borrowAmount = homeCost - downPayment
+    return borrowAmount;
+}
+
+const downPayment = document.querySelector('#down');
+const homeCost = document.querySelector('#home');
+const borrowAmount = getborrowAmount();
+/*Is this circular ^ ? */
+const repayYears =
+const interestRate = 
+const monthlyPayment = /*PMT function*/
+
