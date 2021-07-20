e63952dec065f5a36f01bf0c46058f45493bfb1e
tosha-g <vetagreenstone@gmail.com>
2021-07-20 16:30:35 +0300

    refactor attempt

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 
 let borrowfill = document.getElementById('needToBorrow');
-function getborrowAmount(type = 0){
 let homeCost = document.getElementById('home').value;
 let downPayment = document.getElementById('down').value;
-let borrowAmount = (homeCost - downPayment);
+function getborrowAmount(type = 0){
+    let borrowAmount = (homeCost - downPayment).toPrecision();
 if(type === 0){
 borrowfill.innerHTML = '$' + borrowAmount.toLocaleString();
 } else {
@@ -13,11 +13,9 @@ borrowfill.innerHTML = '$' + borrowAmount.toLocaleString();
 
 let borrowpercentfill = document.getElementById('needToBorrowPercent');
 function getborrowPercent(){
-    let homeCost = document.getElementById('home').value;
-    let downPayment = document.getElementById('down').value;
-    let borrowAmount = (parseInt(homeCost) - parseInt(downPayment));
-    let borrowPercent = (borrowAmount / homeCost) * 100
-    borrowpercentfill.innerHTML = parseInt(borrowPercent) + '%';
+    getborrowAmount();
+    let borrowPercent = (getborrowAmount(1) / homeCost * 100).toPrecision();
+    borrowpercentfill.innerHTML = borrowPercent + '%';
 }
 
 function allTopFunctions(){
@@ -27,71 +25,39 @@ function allTopFunctions(){
 
 document.getElementById('calcButton').addEventListener('click', allTopFunctions)
 
+let number_of_payments = document.getElementById('period').value * 12;
+
 let monthlyFill = document.getElementById('monthlyPayment');
 function pmt(/*rate_per_period, number_of_payments,present_value, future_value, type*/){
-    let rate_per_period = (document.getElementById('rate').value)/100/12;
-    let number_of_payments = document.getElementById('period').value * 12;
-    let present_value = document.getElementById('needToBorrow').value;
+    let rate_per_period = ((document.getElementById('rate').value)/100/12).toPrecision(2);
+    let number_of_payments = (document.getElementById('period').value * 12).toPrecision(2);
+    let present_value = getborrowAmount(1); 
     let future_value = 0;
     let type = 1;
     let monthlyAmount;
     if (rate_per_period != 0.0){
             /* Interest rate exists*/
             let q = Math.pow(1 + rate_per_period, number_of_payments);
-            monthlyFill.innerHTML = '$' + parseInt((rate_per_period * (future_value + (q * present_value))) / ((-1 + q) * (1 + rate_per_period * (type))));
+            monthlyFill.innerHTML = '$' + ((rate_per_period * (future_value + (q * present_value))) / ((-1 + q) * (1 + rate_per_period * (type)))).toPrecision();
     
         } else if(number_of_payments != 0.0){
             /*No interest rate, but number of payments exists*/
-            monthlyFill.innerHTML = '$' + parseInt((future_value + present_value) / number_of_payments);
+            monthlyFill.innerHTML = '$' + ((future_value + present_value) / number_of_payments).toPrecision();
         }
-    
-        /*return 0;
-         monthlyFill.innerHTML = '$' + monthlyAmount;*/
 }
 
 let totalFill = document.getElementById('totalPayment');
+let total = monthlyAmount * number_of_payments;
 function getTotalPayment(){
-    let rate_per_period = (document.getElementById('rate').value)/100/12;
-    let number_of_payments = document.getElementById('period').value * 12;
-    let present_value = document.getElementById('needToBorrow').value;
-    let future_value = 0;
-    let type = 1;
-    let monthlyAmount;
-    if (rate_per_period != 0.0){
-            /* Interest rate exists*/
-            let q = Math.pow(1 + rate_per_period, number_of_payments);
-            monthlyFill.innerHTML = '$' + parseInt((rate_per_period * (future_value + (q * present_value))) / ((-1 + q) * (1 + rate_per_period * (type))));
-    
-        } else if(number_of_payments != 0.0){
-            /*No interest rate, but number of payments exists*/
-            monthlyFill.innerHTML = '$' + parseInt((future_value + present_value) / number_of_payments);
-        }
-    let total = monthlyAmount * number_of_payments 
-totalFill.innerHTML = '$' +  total.toLocaleString();
+    pmt();
+    totalFill.innerHTML = '$' +  total.toLocaleString();
 }
 
 let intFill = document.getElementById('totalIntPayment');
+let int = total - borrowAmount;
 function getIntPayment(){
-    let rate_per_period = (document.getElementById('rate').value)/100/12;
-    let number_of_payments = document.getElementById('period').value * 12;
-    let present_value = document.getElementById('needToBorrow').value;
-    let future_value = 0;
-    let type = 1;
-    let monthlyAmount;
-    if (rate_per_period != 0.0){
-            /* Interest rate exists*/
-            let q = Math.pow(1 + rate_per_period, number_of_payments);
-            monthlyFill.innerHTML = '$' + parseInt((rate_per_period * (future_value + (q * present_value))) / ((-1 + q) * (1 + rate_per_period * (type))));
-    
-        } else if(number_of_payments != 0.0){
-            /*No interest rate, but number of payments exists*/
-            monthlyFill.innerHTML = '$' + parseInt((future_value + present_value) / number_of_payments);
-        }
-    let total = monthlyAmount * number_of_payments 
-    let homeCost = document.getElementById('home').value;
-let downPayment = document.getElementById('down').value;
-let borrowAmount = (homeCost - downPayment);
-    let int = total - borrowAmount
+    getTotalPayment;
+    getborrowAmount();
     intFill.innerHTML = '$' + int.toLocaleString(); 
 }
 
